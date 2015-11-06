371259babfb35b659dbd1937740689ed6caf39f6
Bojie Jiang <jiangbojie@gmail.com>
2015-11-05 23:28:05 -0800

    seekbar seek time works

--- app/src/main/java/com/bojie/eggtimer/MainActivity.java
+++ app/src/main/java/com/bojie/eggtimer/MainActivity.java
@@ -1,13 +1,55 @@
 package com.bojie.eggtimer;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
+
+    SeekBar mSeekBar;
+    TextView timeTextView;
+    Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSeekBar = (SeekBar) findViewById(R.id.seekBar);
+        timeTextView = (TextView) findViewById(R.id.tv_time);
+        mButton = (Button) findViewById(R.id.button);
+
+        mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+
+                String seekedTime = formatMilliseconds(progress);
+                timeTextView.setText(seekedTime);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
+
+    private String formatMilliseconds(int millis) {
+        String formated =String.format("%02d:%02d",
+                TimeUnit.MILLISECONDS.toMinutes(millis),
+                TimeUnit.MILLISECONDS.toSeconds(millis) -
+                        TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis))
+        );
+        return formated;
+    }
+
 }
